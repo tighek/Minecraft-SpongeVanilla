4501df715cd1a7a1535256c80f6247364c279915
Tighe Kuykendall <tighe@buoy.net>
2016-07-30 13:40:04 -0400

    no message

--- build.sv
+++
@@ -1,4 +0,0 @@
-docker build -t tighek/Minecraft-SpongeVanilla:420B343 .
-docker rmi tighek/Minecraft-SpongeVanilla:latest
-docker tag tighek/Minecraft-SpongeVanilla:420B343 tighek/Minecraft-SpongeVanilla:latest
-
